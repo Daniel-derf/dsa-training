9d50e76c64b6138466cd43d80700d440171ac6e7
Daniel-derf <danielpardinho321@gmail.com>
2025-02-27 20:25:53 -0300

    hashmap implemented with tests

--- pratica_1/data_structures/hashmap.js
+++ pratica_1/data_structures/hashmap.js
@@ -4,7 +4,7 @@ class HashMap {
   array = [];
 
   constructor() {
-    for (let i; (i = 0); i < 50) {
+    for (let i = 0; i < 50; i++) {
       this.array.push(undefined);
     }
   }
@@ -16,25 +16,73 @@ class HashMap {
   }
 
   set(key, value) {
-    const idx = this._hashFunction(key.length);
-    this.array[idx] = [key, value];
+    const idx = this._hashFunction(key);
+
+    if (!this.array[idx]) {
+      this.array[idx] = [key, value, []];
+      return;
+    }
+
+    if (this.array[idx][0] === key) {
+      this.array[idx] = [key, value, []];
+      return;
+    }
+
+    let nextValue = this.array[idx][2];
+
+    while (true) {
+      if (!nextValue.length) break;
+      nextValue = nextValue[2];
+    }
+
+    nextValue.push(key);
+    nextValue.push(value);
+    nextValue.push([]);
   }
 
   get(key) {
     const idx = this._hashFunction(key);
 
-    return this.array[idx] ? this.array[idx][1] : undefined;
+    if (!this.array[idx]) return undefined;
+
+    if (this.array[idx][0] === key) return this.array[idx][1];
+
+    let nextValue = this.array[idx][2];
+    while (true) {
+      if (nextValue[0] === key) break;
+      nextValue = nextValue[2];
+    }
+
+    return nextValue[1];
   }
 
   delete(key) {
     const idx = this._hashFunction(key);
-    this.array[idx] = undefined;
+
+    if (!this.array[idx]) return undefined;
+
+    if (this.array[idx][0] === key) {
+      this.array[idx] = this.array[idx][2].length ? this.array[idx][2] : undefined;
+      return;
+    }
+
+    let nextValue = this.array[idx][2];
+    while (true) {
+      if (nextValue[0] === key) break;
+      nextValue = nextValue[2];
+    }
+
+    nextValue = nextValue[2] ? nextValue[2] : undefined;
   }
 }
 
 const hashmap = new HashMap();
 
 hashmap.set("key1", 10);
+assert.deepStrictEqual(hashmap.get("key1"), 10);
+
+hashmap.set("key2", 15);
+assert.deepStrictEqual(hashmap.get("key2"), 15);
 assert.deepStrictEqual(hashmap.get("key1"), 10);
 
 hashmap.set("key1", 20);
